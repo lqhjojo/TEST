ec9934bda30b59a98771eda7554a3b8e664a0853
Your Name <17581858087@163.com>
2024-01-21 10:43:47 +0800

    the first commit

---
+++ src/main.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+
+
+//#define  MAX_STRING_NUM 20
+
+//typedef char string[MAX_STRING_NUM];
+
+//typedef struct person
+//{
+   //int age;
+   //string name;
+
+//}person;
+
+int main(void)
+{
+
+ //person jack={.age = 15,.name="Jack"};
+  printf("1") ;
+
+ return 0;
+
+}
